c23c65f23c43e9146051017cf6f92490e93d9b0a
Gabifd <gabrieli.fd@aluno.ifsc.edu.br>
2026-08-20 09:22:19 -0300

    Esqueleto da solução e da aula1

---
+++ portfolio_mic/aula1_gpio/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 8/20/2026 8:59:00 AM
+ *  Author: Aluno
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
