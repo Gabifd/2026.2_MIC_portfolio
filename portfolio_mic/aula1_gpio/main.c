2713b2f53fe66d0c9d964fbed9568e3828a489d1
Gabifd <gabrieli.fd@aluno.ifsc.edu.br>
2026-08-20 11:34:58 -0300

    EXPERIMENTO COM PINOS DE ENTRADA E CHAVES SIMULAÇÃO RECEBEU UMA CHAVE O PINO pd3 E O SOFTWARE

--- portfolio_mic/aula1_gpio/main.c
+++ portfolio_mic/aula1_gpio/main.c
@@ -12,12 +12,14 @@
 int main(void)
 {
 	DDRB|= 0b00100000; //define pino PB5 como saida
-    while(1)
-    {
+	DDRB &= 0b11110111;  //define pino PD3 como entrada
+    while(1){
+		if((PIND & 0b00001000) !=0){//ler valor da chave PD3, segue se estiver em nivel logico alto
 		PORTB |= 0b00100000; //ATRIBUI NIVEL LOGICO ALTO AO PINO pb5
 		_delay_ms(100);
 		PORTB &= 0b11011111;
 		_delay_ms(900); //atribui nivel logico baixo ao pino pb5
         //TODO:: Please write your application code 
+		}
     }
 }
