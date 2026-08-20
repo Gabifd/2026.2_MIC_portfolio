606ee3c0d796691c165994e2d1f0c86477191301
Gabifd <gabrieli.fd@aluno.ifsc.edu.br>
2026-08-20 11:15:10 -0300

    Código pisca led com mascaras binárias

--- portfolio_mic/aula1_gpio/main.c
+++ portfolio_mic/aula1_gpio/main.c
@@ -5,13 +5,19 @@
  *  Author: Gabi
  */ 
 
+#define F_CPU 16000000
 #include <xc.h>
+#include "util/delay.h"
 
 int main(void)
 {
+	DDRB|= 0b00100000; //define pino PB5 como saida
     while(1)
     {
+		PORTB |= 0b00100000; //ATRIBUI NIVEL LOGICO ALTO AO PINO pb5
+		_delay_ms(100);
+		PORTB &= 0b11011111;
+		_delay_ms(900); //atribui nivel logico baixo ao pino pb5
         //TODO:: Please write your application code 
-		//linha exemplo
     }
 }
