0648720a6c036d8654b11551a17a319268134161
Gabifd <gabrieli.fd@aluno.ifsc.edu.br>
2026-08-27 09:07:15 -0300

    commit 1.1

--- portfolio_mic/aula2_gpio/main.c
+++ portfolio_mic/aula2_gpio/main.c
@@ -16,9 +16,15 @@
 
 int main(void){
 	//3 formas diferentes de setar a direção de PB2 e PB5
+	
+	//forma 1: shifts com definições do fabricante
 	DDRB = (1<<DDB5) | (1<<DDB2);
+	
+	//forma 2: binario puro
 	DDRB = 0b00100000 | 0b00000100;
 	//res 0b00100100
+	
+	//forma 3: 
 	
 	DDRB = BIT5_MASK | BIT2_MASK;
     while(1)
