e3bf737c2669647906e254ae4cdee3afb10a37f3
Gabifd <gabrieli.fd@aluno.ifsc.edu.br>
2026-08-27 10:03:57 -0300

    Codigo de shifts para ccontrole de LEDS utilizados pinos PB0, PB2, PB4, PB6 e logica de shifts estilo "pinheiro e natal

--- portfolio_mic/aula2_gpio/main.c
+++ portfolio_mic/aula2_gpio/main.c
@@ -8,27 +8,26 @@
 #define F_CPU 16000000
 #include <xc.h>
 #include "util/delay.h"
+#include "stdint.h"
 
-#define BIT0_MASK 0b00000001
-#define BIT1_MASK 0b00000010
-#define BIT2_MASK 0b00000100
-#define BIT5_MASK 0b00100000
+//controle de varios leds com operações de shift
 
-int main(void){
-	//3 formas diferentes de setar a direção de PB2 e PB5
-	
-	//forma 1: shifts com definições do fabricante
-	DDRB = (1<<DDB5) | (1<<DDB2);
-	
-	//forma 2: binario puro
-	DDRB = 0b00100000 | 0b00000100;
-	//res 0b00100100
-	
-	//forma 3: 
-	
-	DDRB = BIT5_MASK | BIT2_MASK;
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+void GPIO_config() {
+	DDRB = (1<<DDB6)|(1<<DDB4)|(1<<DDB2)|(1<<DDB0); //bits pares sao configurados com
+	//DDRB = 0b01010101;
+	//se são entradas, têm alta impedância
+}
+
+int main(void) {
+	GPIO_config();
+	uint8_t tData = 0x03; //0b0000 0011
+	while (1) {
+		PORTB = tData;
+		//descobrir o DDB e o PORTB para saber o que vai acontecer efetivamente
+		_delay_ms(1000);
+		tData = tData << 1 ; //0b0000 0110, 0b0000 1100, 0b0001 1000, ...
+		if (tData == 0){
+			tData = 0x03;
+		}
+	}
 }
