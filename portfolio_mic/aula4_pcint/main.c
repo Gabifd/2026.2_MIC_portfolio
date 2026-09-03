ec48247d15107d030439cdc2bb4e8b2a5b15f35b
Gabifd <gabrieli.fd@aluno.ifsc.edu.br>
2026-09-03 11:17:35 -0300

    logica de eventos press/releasena tecla w

--- portfolio_mic/aula4_pcint/main.c
+++ portfolio_mic/aula4_pcint/main.c
@@ -7,9 +7,17 @@
 
 #define F_CPU 16000000
 #include <xc.h>
+#include <stdint.h>
 #include "avr/interrupt.h"
 #include "util/delay.h"
 
+#define KEY_RELEASED 0
+#define KEY_PRESSED 1
+
+uint8_t gKeyState_w = 0;
+uint8_t gKeyState_a = 0;
+uint8_t gKeyState_s = 0;
+uint8_t gKeyState_d = 0;
 
 void GPIO_config(){
 	DDRB &= ~((1<<DDB0)|(1<<DDB1)|(1<<DDB2)|(1<<DDB3)); //Define as portas PB0 PB1 PB2 PB3	 COMO ENTRADA- seta elas como 1, o << shifta a esquerda, ~inverte 0 pra 1 e 1 pra 0 e & multiplica a & a mascara pelo original, setando  1111 0000 (ja é originalmente assim, é apenas uma boa pratica)
@@ -20,7 +28,7 @@ void GPIO_config(){
 	}
 
 void GPIO_incBar() {
-	PORTD = PORTD>>1;
+	PORTD = PORTD >> 1;
 	PORTD |= 0b10000000; //Aciona bit mais significativo
 }
 
@@ -29,11 +37,28 @@ void PCINT_config(){
 	PCMSK0 |= (1<<PCINT3)|(1<<PCINT2)|(1<<PCINT1)|(1<<PCINT0); //Habilita interrupção PCINT0-PCINT7. se no lugar de 1 colocar 0, desabilita a interrupção
 }
 
-
 ISR(PCINT0_vect){
-	PORTC |= (1<<PORTC0); //seta pino PC0
-	_delay_ms(100);
-	PORTC &= ~(1<<PORTC0); //zera pino PC0
+	uint8_t tCurrentKeyState_w = 0;
+	if ((PINB & (1<<PINB0)) != 0){ //Testa pino PB0
+		//PB0=1, tecla w solta
+		tCurrentKeyState_w = KEY_RELEASED;
+	}  else {
+		//pb0 = 0, tecla w pressionada
+		tCurrentKeyState_w = KEY_PRESSED;
+	}
+	 if(tCurrentKeyState_w == KEY_PRESSED && gKeyState_w == KEY_RELEASED){
+		 // tecla w acabou de ser pressionada
+		gKeyState_w = KEY_PRESSED; 
+			GPIO_incBar();
+	 } else 
+	 if(tCurrentKeyState_w == KEY_RELEASED && gKeyState_w == KEY_PRESSED){
+		 // tecla w acabou de ser solta
+		 gKeyState_w = KEY_RELEASED;
+	 }
+	 
+	PORTC ^= (1<<PORTC0); //seta pino PC0
+	//_delay_ms(100);
+	//PORTC &= ~(1<<PORTC0); //zera pino PC0
 }
 int main(void){
 	GPIO_config();
