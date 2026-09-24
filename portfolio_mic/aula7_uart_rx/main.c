ccc3a06f8a92b1be9ee3f9e3c72e57e819deb08c
Gabifd <gabrieli.fd@aluno.ifsc.edu.br>
2026-09-24 11:37:12 -0300

    Tratamento de buffer de mensagens na UART

--- portfolio_mic/aula7_uart_rx/main.c
+++ portfolio_mic/aula7_uart_rx/main.c
@@ -7,8 +7,16 @@
 
 #define F_CPU 16000000
 #include <xc.h>
+#include <string.h>
 #include "util/delay.h"
 #include "avr/interrupt.h"
+
+#define RX_BUFFER_SIZE 16
+
+uint8_t gMessage[16];								//buffer global da uart
+uint16_t gRxCounter = 0;							//contagem de bytes recebidos
+uint8_t gMessageReady = 0;
+
 /**
 * Configuração do modulo USART0 para modo assincrono, frame de 8 bits, paridade par
 **/
@@ -19,7 +27,7 @@ void UART_config_rx(uint16_t pBAUD){
 	UCSR0B = (1<<RXEN0)|(0<<TXEN0)|(0<<UCSZ02)		//Habilita o receptor UART0, frame de 8 bits
 	| (1<<RXCIE0);									//Habilita interrumpção da recepção da UART0
 	UCSR0C = (0<<UMSEL01)|(0<<UMSEL00)				//modo assíncrono
-	|(1<<UPM01)|(1<<UPM00)							//Habilita paridade par
+	|(1<<UPM01)|(0<<UPM00)							//Habilita paridade par
 	|(1<<USBS0)										//2 bits de stop
 	|(1<<UCSZ01)|(1<<UCSZ00)						//Frame de 8 bits
 	|(0<<UCPOL0);									//polaridade clock: ignorada
@@ -30,15 +38,15 @@ void GPIO_config(){
 }
 
 ISR(USART_RX_vect){
-	       uint8_t tReceivedByte = UDR0;				//Leitura do buffer UART
-	       //Tratamento da mensagem recebida
-	       if(tReceivedByte == 'M'){
-		       PORTC |= (1<<PORTC0);					//LED VERDE
-		       } else {
-		       PORTC |= (1<<PORTC1);						//LED VERMELHO
-	       }
-	       _delay_ms(1);
-	       PORTC = 0;									//Apaga todos os LEDs
+	      uint8_t tReceivedByte = UDR0;				//Leitura do buffer UART
+		  gMessage[gRxCounter] = tReceivedByte;		//armazena bytes recebidos no buffer
+		  gRxCounter++;
+		  if(gRxCounter == RX_BUFFER_SIZE) {//Proteção contra estouro do buffer
+		  gRxCounter = 0;
+		  }
+		  if(tReceivedByte == '\n'){
+			  gMessageReady = 1;					//Flag de mensagem completa
+		  }
 }
 
 int main(void) {
@@ -46,6 +54,17 @@ int main(void) {
 	UART_config_rx(9600);
 	sei();												//habilta interrupções globalmente
     while(1) {
-//
-	}
+		if(gMessageReady){								//aguarda mensagem completa
+			gMessageReady = 0;
+		
+		//Tratamento da mensagem recebida
+if(strcmp((char*)gMessage, "Message") == 0){
+	PORTC |= (1<<PORTC0);					//LED VERDE
+	} else {
+	PORTC |= (1<<PORTC1);						//LED VERMELHO
+}
+_delay_ms(10);
+PORTC = 0;									//Apaga todos os LEDs
+}
+}
 }
